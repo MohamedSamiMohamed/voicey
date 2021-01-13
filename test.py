61621123c9421a437d7d115e6c3958f36453a38e
MohamedSamiMoahmed <mohamed.samy9956@gmail.com>
2021-01-13 21:20:07 +0200

    low pass filter is  applied and working

--- test.py
+++ test.py
@@ -20,11 +20,17 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     y = lfilter(b, a, data)
     return y
 
-fs = 40000.0
-lowcut = 1000
-highcut = 10000
 
-# Plot the frequency response for a few different orders.
+
+
+lowcut = 500.0
+highcut = 1250.0
+
+fs = 5000.0
+T = 0.05
+nsamples = int(T * fs)
+t = np.linspace(0, T, nsamples, endpoint=False)
+#Plot the frequency response for a few different orders.
 plt.figure(1)
 plt.clf()
 for order in [3, 6, 12]:
@@ -38,4 +44,37 @@ plt.xlabel('Frequency (Hz)')
 plt.ylabel('Gain')
 plt.grid(True)
 plt.legend(loc='best')
+plt.show()
+
+# a = 0.02
+# f0 = 600.0
+# x = 0.1 * np.sin(2 * np.pi * 1.2 * np.sqrt(t))
+# x += 0.01 * np.cos(2 * np.pi * 312 * t + 0.1)
+# x += a * np.cos(2 * np.pi * f0 * t + .11)
+# x += 0.03 * np.cos(2 * np.pi * 2000 * t)
+# plt.figure(2)
+# plt.clf()
+# plt.plot(t, x, label='Noisy signal')
+
+
+
+
+# y = butter_bandpass_filter(x, lowcut, highcut, fs, order=6)
+# plt.plot(t, y, label='Filtered signal (%g Hz)' % f0)
+# plt.show()
+# freq = fftpack.fftfreq(t.shape[-1])
+# x_freq=fftpack.fft(x)
+# plt.plot(freq, x_freq.real)
+# plt.show()
+# y_freq=fftpack.fft(y)
+# plt.plot(freq, y_freq.real)
+# plt.show()
+
+# plt.plot(t, y, label='Filtered signal (%g Hz)' % f0)
+# plt.xlabel('time (seconds)')
+# plt.hlines([-a, a], 0, T, linestyles='--')
+# plt.grid(True)
+# plt.axis('tight')
+# plt.legend(loc='upper left')
+
 plt.show()
--- voicey.py
+++ voicey.py
@@ -13,16 +13,15 @@ samplerate_list=[]
 freq_signals=[]
 modulated_signal=[]
 modulating_signal=[]
-def butter_bandpass(lowcut, highcut, fs, order=5):
+def butter_bandpass(lowcut, fs, order):
     nyq = 0.5 * fs
     low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band')
+    b, a = butter(order, low, btype='low',analog=False)
     return b, a
 
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+def butter_bandpass_filter(data, lowcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
 def read_signal(path):
@@ -32,8 +31,10 @@ def read_signal(path):
         data=np.array((un_modulated[:,0]+un_modulated[:,1])/2,dtype=np.int16)
     else:
         data=un_modulated
+    #sample rate=number of samples(data.shape[0])/overall time of the wave    
     T=data.shape[0]/samplerate
-    frequency=1/T
+    #frequency=1/T
+    #time from 0 to T(10 sec for our inputs) and has data.shape[0] samples(bins)
     t.append(np.linspace(0.,T,data.shape[0]))
     x.append(data)
     shape.append(data.shape[0])
@@ -43,55 +44,46 @@ read_signal("output2.wav")
 read_signal("output3.wav")
 read_signal("output4.wav")
 min_shape=min(shape)
-
+print(shape)
 for i in range(0,3):
     x[i]=x[i][0:min_shape]
     t[i]=t[i][0:min_shape]
 
-
-# plt.plot(t[0],x[0])
-# plt.show()
+freq_1 = fftpack.fft(x[0])
+freq = fftpack.fftfreq(min_shape) * samplerate_list[0]
+#wavfile.write("example.wav",samplerate_list[0],x[0])
+plt.plot(t[0],x[0])
+plt.show()
 # plt.plot(t[0],x[1])
 # plt.show()
 # plt.plot(t[0],x[2])
 # plt.show()
-modulating_signal.append(np.cos(2 * np.pi * 5000 * t[0]))
-modulating_signal.append(np.cos(2 * np.pi * 20000 * t[0]))
-modulating_signal.append(np.sin(2 * np.pi * 20000 * t[0]))
+modulating_signal.append(np.cos( 5000 * t[0]))
+modulating_signal.append(np.cos( 80000 * t[0]))
+modulating_signal.append(np.sin( 80000* t[0]))
 for i in range(0,3):
     modulated_signal.append(np.multiply(x[i],modulating_signal[i]))
+s_t=modulated_signal[0]+modulated_signal[1]+modulated_signal[2]
 
 freq = fftpack.fftfreq(min_shape) * samplerate_list[0]
+#freq = fftpack.fftfreq(min_shape)
 for i in range(0,3):
     freq_signals.append(fftpack.fft(modulated_signal[i]))
-
-plt.plot(freq, freq_signals[0].real,freq, freq_signals[1].real,freq, freq_signals[2].real)
+freq_signals.append(fftpack.fft(s_t))
+s_t_freq=fftpack.fft(s_t)
+plt.plot(freq, s_t_freq.real)
+plt.show()
+print((1700/2*np.pi))
+filtered=butter_bandpass_filter(s_t,int(5000/2*np.pi),samplerate_list[0],3)
+filtered_freq=fftpack.fft(filtered)
+plt.plot(freq, filtered_freq.real)
+plt.show()
+x_t=filtered*modulating_signal[0]
+x_t=butter_bandpass_filter(x_t,int(5000/2*np.pi),samplerate_list[0],3)
+x_t=np.array(x_t,dtype=np.int16)
+plt.plot(t[0],x_t)
+wavfile.write("example.wav",samplerate_list[0],x_t)
 plt.show()
 
 
-
-
-
-
-
-
-# freq_signal=fftpack.fft(modulated_signal)
-# freq = fftpack.fftfreq(modulated_signal.shape[0]) * samplerate
-
-# plt.plot(freq, freq_signal.real)
-# plt.show()
-
-
-# for order in [3, 6, 12]:
-#     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-#     w, h = freqz(b, a, worN=2000)
-#     plt.plot((fs * 0.5 / np.pi) * w, abs(h), label="order = %d" % order)
-
-# plt.plot([0, 0.5 * fs], [np.sqrt(0.5), np.sqrt(0.5)],
-#             '--', label='sqrt(0.5)')
-# plt.xlabel('Frequency (Hz)')
-# plt.ylabel('Gain')
-# plt.grid(True)
-# plt.legend(loc='best')
-# plt.show()
 
